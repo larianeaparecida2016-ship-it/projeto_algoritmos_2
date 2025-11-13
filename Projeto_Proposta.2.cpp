b7a4e1f2d9de0fb37535159ccc943636986b5747
larianeaparecida2016-ship-it <larianeaparecida2016@gmail.com>
2025-11-13 15:51:10 -0300

    Update Projeto_Proposta.2.cpp

--- Projeto_Proposta.2.cpp
+++ Projeto_Proposta.2.cpp
@@ -1,104 +1,58 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
+
 struct Cliente {
-    string id;
+    int id;
     string nome;
     string telefone;
 };
 
+
 struct Filme {
-    string codigo;
+    int codigo;
     string titulo;
     string genero;
-    string ano;
-    string idCliente; 
-
+    int ano;
+    int idCliente;
 };
 
-void cadastrarCliente(Cliente clientes[], &totalClientes) {
-Cliente c;
-cout << "ID do cliente: ";
-cin >> c.id;
-
-for (int i = 0; i < totalClientes; i++) { //Verificar se já consta o ID cadastrado de cliente
-    if (clientes[i].id == c.id) {
-        cout << "ID ja consta como cadastrado!\n";
-        return;
-    }
+void mostraMenu(){
+    cout << "Menu de opcoes:\n";
+    cout << "1. Cadastrar cliente.\n";
+    cout << "2. Cadastra filme.\n";
+    cout << "3. Listar clientes.\n";
+    cout << "4. Listar filmes.\n";
+    cout << "5. Buscar filmes por clientes.\n";
+    cout << "6. Relatorio geral.\n";
+    cout << "7. Sair.\n";
 }
 
-void cadastrarFilme(Filme filmes[], int &qtdFilmes, Cliente clientes[], int qtdClientes) {
-    Filme f;
-    cout << "Codigo do filme: ";
-    cin >> f.codigo;
-for (int i = 0; i < qtdFilmes; i++) {
-        if (filmes[i].codigo == f.codigo) {
-            cout << "Codigo ja cadastrado!\n";
+
+void cadastrarCliente(Cliente clientes[], int &qtdClientes) {
+    Cliente c;
+    cout << "ID do cliente: ";
+    cin >> c.id;
+
+    // verifica se já tem esse id
+    for (int i = 0; i < qtdClientes; i++) {
+        if (clientes[i].id == c.id) {
+            cout << "ID ja cadastrado!\n";
             return;
         }
     }
 
-//Continuar a partir daqui .........
-
-
-
-void mostrar(Cliente vet[]) {
-    for(int i=0; i<pos; i++) {
-        cout << "Informacoes cadastro de cliente: " << i+1 << endl;;
-        cout << "ID: " << vet[i].id << endl;
-        cout << "Nome: " << vet[i].nome << endl;
-        cout << "Telefone: " << vet[1].telefone << endl;
-    }
+    cout << "Nome: ";
+    cin.ignore();
+    getline(cin, c.nome);
+    cout << "Telefone: ";
+    getline(cin, c.telefone);
+    clientes[qtdClientes] = c; // Adiconar o cliente na qtdClientes
+    qtdClientes++;
 }
 
-void mostrar(Filme vet[]) {
-    for(int i=0; i<=pos; i++) {
-        cout << "Informacoes cadastro de filmes: " << i+1 << endl;;
-        cout << "Codigo: " << vet[i].codigo << endl;
-        cout << "Nome do filme: " << vet[i].titulo << endl;
-        cout << "Genero do filme: " << vet[1].genero << endl;
-        cout << "Ano: " << vet[1].ano << endl;
-    }
-}
-
-int main() {
-    Cliente esteCliente;
-    Cliente vetorCliente[N];
-    Filme esteFilme;
-    Filme vetorFilme;
-
-    int opcao;
-
-
-    mostraMenu();
-    cin >> opcao;
-    while (opcao != 0) {
-        switch(opcao) {
-        case 1:
-            cadastrar (&esteCliente)
-            adicionarCliente (vetorCliente, esteCliente);
-            break;
-        case 2:
-            cadastrar (&esteFilme)
-            adicionarFilme (vetorFilme, esteFilme);
-            break;
-        case 3:
-            mostrar (vetorCliente);
-            break;
-        case 4:
-            mostrar (vetorFilme);
-
-        }
-    }
-
-
-return 0;
-
-}
-
+// COntinuar daqui .... (cadastrar filme)
 
 
 
